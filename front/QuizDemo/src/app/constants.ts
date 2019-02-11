35d734c9036adb73d9719055caace25b2a6824d0
Nishant Agarwal <nishant.agarwal@cybergindia.local>
2019-02-11 12:04:27 +0530

    Minor update added constant

--- front/QuizDemo/src/app/constants.ts
+++ front/QuizDemo/src/app/constants.ts
@@ -1,3 +1,5 @@
 export class Constants {
-    public static get SERVICE_URL(): string { return "http://localhost:5000"; };
+    // public static get SERVICE_URL(): string { return "http://localhost:5000"; };
+    public static get SERVICE_URL(): string { return "http://172.25.122.125:5000"; };
+    
   }
